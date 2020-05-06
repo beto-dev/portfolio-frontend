ca6fc31762af3400e3e936518a5707a8efaff6b6
beto-dev <alb.tejos.dev@gmail.com>
2020-05-06 11:54:14 -0400

    Example of api axios request

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,32 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <Portfolio msg="An amazing Vue project"/>
+    <p>Consultando... {{ url }}</p>
+    <p>Info recibida <span>&#128071;</span></p>
+    <pre id="api-info">{{ info }}</pre>
   </div>
 </template>
 
 <script>
 import Portfolio from './components/Portfolio.vue'
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
     Portfolio
+  },
+  data () {
+    return {
+      url: null,
+      info: null
+    }
+  },
+  mounted () {
+    this.url = 'http://localhost:8081/saludo';
+    axios
+      .get(this.url)
+      .then(response => (this.info = response))
   }
 }
 </script>
@@ -25,4 +41,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#api-info {
+  text-align: left;
+}  
 </style>
